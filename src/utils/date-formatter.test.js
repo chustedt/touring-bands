751ebab5ba97e4cca99a4560719230862eeaf6fd
Chris Hustedt <chris.hustedt@gmail.com>
2023-10-31 16:03:25 -0500

    Fix file inconsistancies

--- src/utils/date-formatter.test.js
+++ src/utils/date-formatter.test.js
@@ -1,14 +1,14 @@
 import { formatDate } from "./date-formatter";
 
-describe("formatDate function", () => {
-  it("should return a correctly formatted date string", () => {
+describe("formatDate", () => {
+  test("correctly formatted date string", () => {
     const milliseconds = 1677855600000; // Friday, March 3, 2023
     const formattedDate = formatDate(milliseconds);
 
     expect(formattedDate).toBe("Friday, March 3");
   });
 
-  it("should handle non-number inputs gracefully", () => {
+  test("non-number inputs gracefully handled", () => {
     const result = formatDate("some string");
     expect(result).toBe("Invalid Date");
   });
